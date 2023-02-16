d485fee050edd8c50e2a0ef6db2fa0959f51601f
luisreyesredmi <luisreyesredmi@gmail.com>
2023-02-15 21:12:10 -0400

    apis.b

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,24 +1,71 @@
 import style from "./Content.module.css";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { AiOutlineMinusCircle } from "react-icons/ai";
-import foods from "../constants/foods";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
-const Content = () => {
-  const { id } = useParams();
+const Content = (props) => {
+  const cont = [
+    {
+      id: 2,
+      name: "Morty Smith",
+      status: "Alive",
+      species: "Human",
+      type: "",
+      gender: "Male",
+      origin: {
+        name: "Earth",
+        url: "https://rickandmortyapi.com/api/location/1"
+      },
+      location: {
+        name: "Earth",
+        url: "https://rickandmortyapi.com/api/location/20"
+      },
+      image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+      episode: [
+        "https://rickandmortyapi.com/api/episode/1",
+        "https://rickandmortyapi.com/api/episode/2",
+      ],
+      url: "https://rickandmortyapi.com/api/character/2",
+      created: "2017-11-04T18:50:21.651Z"
+    }
+  ];
+  const contMenu = cont.find(({id}) => id === 2);
+  console.log(contMenu);
 
-  const myFood = foods.find((food) => food.id === id);
+  const contFilter = cont.filter(characters => characters.id === 2);
+  console.log(contFilter);
+  
+  
 
-  if (myFood === undefined) {
+  const [characters, setCharacters] = useState([]);
+  const url = "https://rickandmortyapi.com/api/character/";
+
+  
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(url, requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        setCharacters(result.results);
+      })
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  if (characters === undefined) {
     return <p>No encontré la comida que buscabas</p>
   }
   return (
     <section className={style.ContainerContent}>
-      <img src={myFood.imagen} width="500" height="325"/>
+      <img src={characters.image} width="500" height="325"/>
       <div className={style.ContainerText}>
-        <h2 className={style.TheBasicContent}>{myFood.name}</h2>
-        <p className={style.WordsContent}>{myFood.text}</p>
-        <div className={style.CostContent}>${myFood.precio}</div>
+        <h2 className={style.TheBasicContent}>{characters.name}</h2>
+        <p className={style.WordsContent}>{characters.text}</p>
+        <div className={style.CostContent}>${characters.precio}</div>
         <div className={style.AmountContent}>
           <button className={style.Icons1}>
             <AiOutlinePlusCircle />
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,7 +1,6 @@
 import style from "./Grid.module.css";
 import React, { useEffect, useState } from "react";
 import Card from "./Card";
-import foods from "../constants/foods";
 
 const Grid = ({}) => {
   const [characters, setCharacters] = useState([]);
@@ -15,28 +14,22 @@ const Grid = ({}) => {
 
     fetch(url, requestOptions)
       .then((response) => response.json())
-      .then((result) => {setCharacters(result.results)})
+      .then((result) => {
+        setCharacters(result.results);
+      })
       .catch((error) => console.log("error", error));
   }, []);
 
-  const foodList = foods.map((item) => (
-    <Card
-      nombre={item.name}
-      precio={item.precio}
-      imagen={item.imagen}
-      id={item.id}
-    />
-  ));
   return (
     <div className={style.Main} style={{ display: "flex" }}>
-      {characters.map((card) => {
-        return (
-          <div key={card.id} >
-            <h2> {card.name} </h2>
-            <img src={card.image} alt={card.name} />
-          </div>
-        )
-      })}
+      {characters.map((character) => (
+        <Card
+          nombre={character.name}
+          precio={character.id}
+          imagen={character.image}
+          id={character.id}
+        />
+      ))}
     </div>
   );
 };
